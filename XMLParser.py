b66494bee6c4153bc3c732e6883aa6707b9b880c
nirav151 <nirav.sharda@gmail.com>
2015-10-13 17:59:18 -0500

    Added few more comments

--- XMLParser.py
+++ XMLParser.py
@@ -1,16 +1,39 @@
+#Author : Nirav Sharda
+#Team : Rome
+# This program parses the input file , filters the senteces removing punctuations
+# and stopwords and then clusters the sentences and outputs feature words,
+# which are then used by SenseDef.py for generating meaning and examples.
+# We maintain a list of yet to be clustered sentences. And the idea behind
+# the baseline system is to pick the first yet to be clustered sentence and
+# then find feature words around the target word in that sentences and then
+# look for those feature words in a larger domain of words around the target
+# words in all other sentences and cluster them in the same cluster. Then it
+# also add the feature words for all those sentences clustered and again looks
+# in all the remaining sentences for all these words and tries to add more
+# sentences into this cluster already created. It will keep on doing that
+# till it reaches a stage where there are a very few remaining sentences with
+# no matching feature words. Then it clusters all those into 1 cluster. This
+# is the baseline idea, we have to improve on this in the next stage.
+
+# Import nltk, stopwords from nltk, etree used for parsing
+# Import Counter used for counting feature words 
 import nltk
 from nltk.corpus import stopwords
 from lxml import etree
 from collections import Counter
 
+# Class definition
 class XMLParser(object):
 
+        # Constructor
         def __init__(self):
                 self.data =[]
 
+        #
         def isalfa(self,w):
                 return w.replace('_','').isalpha()
-        
+
+        # 
         def parse(self,path,keyword):
 
                 t,data=self.parsegeneral(path)
@@ -25,7 +48,7 @@ class XMLParser(object):
 
                 target_word = keyword1[0:1]+'_'+keyword2[0:1]
                 return target_word,t,data
-
+        # 
         def parsegeneral(self,path):
                 
                 parser = etree.XMLParser(recover=True)
@@ -97,6 +120,7 @@ class XMLParser(object):
                         feature=[]
                         
                         # Adding first sentence to group of sentences
+                        # Sentences stores the sentences that are part of the same cluster
                         sentences=[remainingsentences[0]]
 
                         # Iterate two times
@@ -114,7 +138,12 @@ class XMLParser(object):
 
                                         #Convert list to int
                                         index_of_target_word=index_of_target_word[0]
+
+                                        # Look for feature words in +- 10 words around the target word in all remaining
+                                        # sentences yet to be clustered.
                                         w=t[remainingsentences[x]][index_of_target_word-10:index_of_target_word+10]
+
+                                        # if some matching words are found add the sentence to the current cluster
                                         if len([words for words in w if words in feature]) !=0:
                                              sentences.append(remainingsentences[x])
 
@@ -122,12 +151,17 @@ class XMLParser(object):
                         sentences=set(sentences)
                         # Convert back to list
                         sentences=list(sentences)
-                        
+
+                        # If the number of sentences in the cluster is 1 break from the loop
+                        # and cluster all the remaining sentences into 1 cluster
                         if len(sentences)==1:
                                 break;
 
+                        # When the number of sentences in the cluster is not 1 update the
+                        # result in a dictionary cluster_information
                         if len(sentences)!=1:
                                 for c in range(len(sentences)):
+                                        # Adding to dictionary 
                                         cluster_information.update({sentences[c]:cluster_number})
                                         
                                 # Adding feature of this cluster to the list features_of_all_clusters
@@ -143,23 +177,36 @@ class XMLParser(object):
                 for x in range(len(remainingsentences)):
                         cluster_information.update({remainingsentences[x]: cluster_number})
                         
+                # Adding feature words of the final cluster to the list features_of_all_clusters
                 features_of_all_clusters.append(self.findfeaturewords(remainingsentences,t,target_word))
-                                                              
+
+                # return cluster_information : dictionary containing clusters sentences
+                # and features_of_all_clusters : a list of lists containing feature words
+                # of all cluster
                 return cluster_information,features_of_all_clusters
 
+        # This function finds the feature words around the target word i.e words in the range +-
+        # 4 words around the target word.
         def findfeaturewords(self,list_of_sentences,t,target_word):
+                
                 # A list for storing feature words
                 feature=[]
                 for x in range(len(list_of_sentences)):
+                                        # find the index of target_word
                                         index_of_target_word=[index for index, word in enumerate(t[list_of_sentences[x]]) if word.startswith(target_word)]
+                                        
                                         #convert list to int
                                         index_of_target_word = index_of_target_word[0]
+
+                                        # Add words to the left of target word to the list feature.
                                         for y in range(index_of_target_word-4,index_of_target_word):
                                                 feature.append(t[list_of_sentences[x]][y])
 
+                                        # Add words to the right of target word to the list feature.
                                         for z in range(index_of_target_word+1,index_of_target_word+5):
                                                 feature.append(t[list_of_sentences[x]][z])
-                
+                                                
+                # return feature : A list containing feature words of all clusters
                 return feature
 
                 
